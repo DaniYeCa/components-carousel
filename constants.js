2921aa22a47b940985b06276244a29e428f0a974
daniyeca <daniel.dance1912@gmail.com>
2023-07-03 18:13:07 +0200

    finished my component carousel

---
+++ constants.js
@@ -0,0 +1,22 @@
+export const IMAGES = [
+  {
+    src: 'images/mariola-grobelska-8a7ZTFKax_I-unsplash.jpg',
+    alt: 'leon',
+  },
+  {
+    src: 'images/rob-potter-Jt16Nz5wRJY-unsplash.jpg',
+    alt: 'tigre',
+  },
+  {
+    src: 'images/pouya-saadati-Dnb1lieK8lQ-unsplash.jpg',
+    alt: 'pasisaje-1',
+  },
+  {
+    src: 'images/vitalik-vynarchyk-lAHzPBi8KoI-unsplash.jpg',
+    alt: 'paisaje-2',
+  },
+  {
+    src: 'images/salman-sidheek-Tmz1HwqtXxA-unsplash.jpg',
+    alt: 'mar',
+  },
+];
